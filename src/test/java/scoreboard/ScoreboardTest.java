f8e23c27548965b7ba2bffe4ca0a3b532664cb8c
Jan Bartnik <jan.bartnik@gmx.com>
2024-07-30 15:04:11 +0200

    Test to prevent the same teams in scoreboard in two matches

--- src/test/java/scoreboard/ScoreboardTest.java
+++ src/test/java/scoreboard/ScoreboardTest.java
@@ -1,23 +1,44 @@
 package scoreboard;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ScoreboardTest {
 
+    private Scoreboard scoreboard;
+    @BeforeEach
+    public void setUp() {
+        scoreboard = new Scoreboard();
+    }
+
     @Test
     public void shouldAddMatchWithZeroScoreToScoreboard() {
         //given
-        Scoreboard scoreboard = new Scoreboard();
-
         //when
         scoreboard.addNewMatch("TeamHome", "TeamAway");
 
         //then
-
         assertEquals(1, scoreboard.getMatches().size());
         assertEquals(0, scoreboard.getMatches().getFirst().getHomeScore());
         assertEquals(0, scoreboard.getMatches().getFirst().getAwayScore());
+    }
+
+    @Test
+    public void shouldNotAddMatchWithExistingTeamsToScoreboard() {
+        //given
+        scoreboard.addNewMatch("TeamOne", "TeamTwo");
+
+        //when
+        scoreboard.addNewMatch("TeamOne", "TeamTwo");
+        scoreboard.addNewMatch("TeamTwo", "TeamOne");
+        scoreboard.addNewMatch("TeamOne", "TeamThree");
+        scoreboard.addNewMatch("TeamTwo", "TeamFour");
+
+
+        //then
+        assertEquals(1, scoreboard.getMatches().size());
+
     }
 }
